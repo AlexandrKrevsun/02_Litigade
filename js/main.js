ccaecda9438930d7ce06e0d0e3e94bef06811fe7
Alexandr_Krevsun <krevsuntaganrog2@hotmail.com>
2022-08-26 15:37:55 +0300

    ready landing page

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function () { 
+    const header = document.querySelector('.header'); /*возвращает 1-й элемент с тем селектором имя которого мы передадим */
+    window.onscroll = ()=>{
+        if (window.pageYOffset > 50) { /*если прокрутка > 50 точек то запускаем класс 'header_active'*/
+            header.classList.add('header_active');
+        }else{
+            header.classList.remove('header_active'); /*иначе удаляем класс*/
+        }
+    }
+}());
