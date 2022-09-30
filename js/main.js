f8f1abd371eebe3ae987347b153506cd53565e95
Alexandr_Krevsun <krevsuntaganrog2@hotmail.com>
2022-09-30 16:28:45 +0300

    new burger, fix clases style.css, video and footer

--- js/main.js
+++ js/main.js
@@ -1,34 +1,14 @@
+
 (function () { 
     const header = document.querySelector('.header'); /*возвращает 1-й элемент с тем селектором имя которого мы передадим */
     window.onscroll = ()=>{
+        
         if (window.pageYOffset > 50) { /*если прокрутка > 50 точек то запускаем класс 'header_active'*/
             header.classList.add('header_active');
         }else{
             header.classList.remove('header_active'); /*иначе удаляем класс*/
         }
     }
+    
 }());
 
-// burger handler
-(function(){
-    const burgerItem = document.querySelector('.burger');
-    const menu = document.querySelector('.header__nav')
-    burgerItem.addEventListener('click',()=> {
-        menu.classList.add('header__nav_active');
-    });
-
-    const menuCloseItem = document.querySelector('.header__nav-close');
-    menuCloseItem.addEventListener('click',()=>{
-        menu.classList.remove('header__nav_active');
-    });
-//убираем меню при нажатии на ссылку
-    const menuLinks = document.querySelectorAll('.header__link');
-    if(window.innerWidth <= 767){
-        for (let i = 0; i < menuLinks.length; i+=1){
-
-        menuLinks[i].addEventListener('click',()=>{
-        menu.classList.remove('header__nav_active')
-            });
-        }
-    }
-}());
